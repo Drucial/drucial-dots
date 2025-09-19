2941b9887468b536a43764718afbb80b8b8913f5
Drucial <drucial@Mac.attlocal.net>
2025-09-19 03:22:28 -0400

    fix lsp in nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,108 +1,82 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    ---@class PluginLspOpts
-    local ret = {
-      -- options for vim.diagnostic.config()
-      ---@type vim.diagnostic.Opts
-      diagnostics = {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = false, -- Disable virtual text for VSCode-style diagnostics
-        severity_sort = true,
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
-            [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
-            [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
-            [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
-          },
-        },
-        float = {
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
+  opts = function(_, opts)
+    -- Keep whatever LazyVim sets, then layer your changes on top
+
+    -- Ensure folds table exists so LazyVim doesn't crash
+    opts.folds = opts.folds or { enabled = false }
+
+    -- diagnostics
+    opts.diagnostics = vim.tbl_deep_extend("force", opts.diagnostics or {}, {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = false, -- VSCode-style: no inline text
+      severity_sort = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+          [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+          [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+          [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
         },
       },
-      -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-      -- Be aware that you also will need to properly configure your LSP server to
-      -- provide the inlay hints.
-      inlay_hints = {
-        enabled = false,
-        exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+      float = {
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
       },
-      -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
-      -- Be aware that you also will need to properly configure your LSP server to
-      -- provide the code lenses.
-      codelens = {
-        enabled = false,
-      },
-      -- add any global capabilities here
-      capabilities = {
-        workspace = {
-          fileOperations = {
-            didRename = true,
-            willRename = true,
-          },
+    })
+
+    -- inlay hints
+    opts.inlay_hints = vim.tbl_deep_extend("force", { enabled = false, exclude = { "vue" } }, opts.inlay_hints or {})
+
+    -- codelens
+    opts.codelens = vim.tbl_deep_extend("force", { enabled = false }, opts.codelens or {})
+
+    -- capabilities
+    opts.capabilities = vim.tbl_deep_extend("force", opts.capabilities or {}, {
+      workspace = {
+        fileOperations = {
+          didRename = true,
+          willRename = true,
         },
       },
-      -- options for vim.lsp.buf.format
-      -- `bufnr` and `filter` is handled by the LazyVim formatter,
-      -- but can be also overridden when specified
-      format = {
-        formatting_options = nil,
-        timeout_ms = nil,
-      },
-      -- LSP Server Settings
-      ---@type lspconfig.options
-      servers = {
-        lua_ls = {
-          -- mason = false, -- set to false if you don't want this server to be installed with mason
-          -- Use this to add any additional keymaps
-          -- for specific lsp servers
-          -- ---@type LazyKeysSpec[]
-          -- keys = {},
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              codeLens = {
-                enable = true,
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-              doc = {
-                privateName = { "^_" },
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
+    })
+
+    -- format
+    opts.format = vim.tbl_deep_extend("force", opts.format or {}, {
+      formatting_options = nil,
+      timeout_ms = nil,
+    })
+
+    -- servers
+    opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
+      lua_ls = {
+        settings = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            codeLens = { enable = true },
+            completion = { callSnippet = "Replace" },
+            doc = { privateName = { "^_" } },
+            hint = {
+              enable = true,
+              setType = false,
+              paramType = true,
+              paramName = "Disable",
+              semicolon = "Disable",
+              arrayIndex = "Disable",
             },
           },
         },
       },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
+    })
+
+    -- setup (preserve your table if you add handlers later)
+    opts.setup = opts.setup or {
+      -- ["*"] = function(server, server_opts) end,
     }
-    return ret
+
+    return opts
   end,
 }
-
