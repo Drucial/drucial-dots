9fc9e013ad20a5da5958922d5d7be87bcee53b91
Drucial <drucial@Mac.attlocal.net>
2025-09-20 12:30:09 -0400

    lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -52,6 +52,7 @@ return {
 
     -- servers
     opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
+      -- Lua
       lua_ls = {
         settings = {
           Lua = {
@@ -69,6 +70,65 @@ return {
             },
           },
         },
+      },
+      -- Tailwind CSS
+      tailwindcss = {
+        -- Explicit filetypes keeps it snappy and avoids false positives
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "sass",
+          "less",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "svelte",
+          "astro",
+          "heex",
+          "elixir",
+          "templ",
+          "php",
+          "twig",
+          "blade",
+        },
+        -- If your project has a tailwind.config.* the server will pick it up automatically.
+        -- These settings just make completions great in JS/TS + class helpers.
+        settings = {
+          tailwindCSS = {
+            includeLanguages = {
+              javascript = "javascriptreact",
+              typescript = "typescriptreact",
+            },
+            -- Add any class extraction helpers you use:
+            experimental = {
+              classRegex = {
+                "tw`([^`]*)`", -- twin.macro / tw`...`
+                'tw$begin:math:text$"([^"]*)"%?\\$end:math:text$', -- tw("...")
+                "cva$begin:math:text$([^)]*)\\$end:math:text$", -- cva(...)
+                "clsx$begin:math:text$([^)]*)\\$end:math:text$", -- clsx(...)
+                "classnames$begin:math:text$([^)]*)\\$end:math:text$", -- classnames(...)
+                -- Tailwind v4-style @apply-in-CSS is picked up via CSS filetypes
+              },
+            },
+            validate = true,
+          },
+        },
+        root_dir = function(fname)
+          -- Prefer project roots that actually have Tailwind/config/build files
+          return require("lspconfig.util").root_pattern(
+            "tailwind.config.js",
+            "tailwind.config.cjs",
+            "tailwind.config.ts",
+            "postcss.config.js",
+            "postcss.config.cjs",
+            "postcss.config.ts",
+            "package.json",
+            ".git"
+          )(fname)
+        end,
       },
     })
 
